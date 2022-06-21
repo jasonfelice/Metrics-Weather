3586c4dc3d8cc3f0541e0a5944b381614acb7b85
Jake Felice <jasonfelice0@gmail.com>
2022-06-21 06:37:04 -0700

    Create a component for country

---
+++ src/components/Country.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Country.module.scss';
+
+const Country = (props) => {
+  const { name, number, vector } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <i className={styles.right_arrow} />
+      <img src={vector} alt={`${name} map`} />
+      <div>
+        <h2>{name}</h2>
+        <span>{number}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Country;
+
+Country.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  vector: PropTypes.string.isRequired,
+};
