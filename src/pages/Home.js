7e4771a17cf2fe6493894ce789918ea4292f8cfa
Jake Felice <jasonfelice0@gmail.com>
2022-06-21 01:19:31 -0700

    Create the Home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => (
+  <>
+    <Header />
+    <div>This is the Home Page!</div>
+  </>
+);
+
+export default Home;
