965ccb3695b29a18cf3cb703b1bf556f1cd3b347
Jake Felice <jasonfelice0@gmail.com>
2022-06-21 05:54:03 -0700

    Create Cities page component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
 import Home from './pages/Home';
 import NoPage from './pages/NoPage';
 import Details from './pages/Details';
+import Cities from './pages/Cities';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details" element={<Details />} />
+      <Route path="/cities" element={<Cities />} />
       <Route path="*" element={<NoPage />} />
     </Routes>
   </Router>
---
+++ src/pages/Cities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Cities = () => (
+  <>
+    <Header heading="Cities" />
+    <div>This is Cities Page!</div>
+  </>
+);
+
+export default Cities;
