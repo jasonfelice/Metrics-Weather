af0a25dc7c8ed9ed5799fa2323f3bf204fc41d0a
Jake Felice <jasonfelice0@gmail.com>
2022-06-21 01:18:48 -0700

    Create Details page component

---
+++ src/pages/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Details = () => (
+  <>
+    <Header />
+    <div>This is Details Page!</div>
+  </>
+);
+
+export default Details;
