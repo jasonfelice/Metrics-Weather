736730ce9ba5512904faa63665a68e58e446d625
Jake Felice <jasonfelice0@gmail.com>
2022-06-22 03:57:33 -0700

    Create countries database

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -36,10 +36,15 @@ const Heading = (props) => {
       <h1>{title}</h1>
       {
         (home) ? (
-          <select className={styles.filter}>
-            <option selected disabled>__Filter Countries by Region__</option>
+          <select defaultValue="all" className={styles.filter}>
+            <option value="all" disabled>__Filter Countries by Region__</option>
             {regions.map((region) => (
-              <option key={region.value} value={region.value}>{region.name}</option>
+              <option
+                key={region.value}
+                value={region.value}
+              >
+                {region.name}
+              </option>
             ))}
           </select>
         ) : (
